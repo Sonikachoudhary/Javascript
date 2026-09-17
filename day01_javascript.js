70533106d7965107ff6bcd99ae6a66c508f93ee7
Sonika Choudhary <Sonikachoudhary2006@gmail.com>
2026-09-17 22:59:07 +0530

    Day 01 - JavaScript Basics

---
+++ day01_javascript.js
@@ -0,0 +1,9 @@
+let name = "Sonika";
+let age = 20;
+let course = "IT";
+let skills = "C CPP and HTML CSS";
+
+console.log("My name is " + name);
+console.log("My age is " + age);
+console.log("I am an " + course + " student");
+console.log("I am learning " + skills);
