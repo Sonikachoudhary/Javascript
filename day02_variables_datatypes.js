1dc0fc1872e14c38c0a86043a24fad6ec8dc393b
Sonika Choudhary <Sonikachoudhary2006@gmail.com>
2026-09-22 18:13:55 +0530

    Day 02: JavaScript variables and data types

---
+++ day02_variables_datatypes.js
@@ -0,0 +1,28 @@
+// Day 02 - JavaScript Variables & Data Types
+
+// Variables
+let name = "Sonika";
+let age = 20;
+const college = "Government Engineering College Ajmer";
+
+// Different Data Types
+let studentName = "Sonika";   // String
+let studentAge = 20;          // Number
+let isStudent = true;         // Boolean
+
+// Display values
+console.log("Name:", studentName);
+console.log("Age:", studentAge);
+console.log("College:", college);
+console.log("Is Student:", isStudent);
+
+// Check data types
+console.log(typeof studentName);
+console.log(typeof studentAge);
+console.log(typeof isStudent);
+let course = "JavaScript";
+let day = 2;
+let learning = true;
+console.log("Course:", course);
+console.log("Day", day);
+console.log("Learning", learning);
